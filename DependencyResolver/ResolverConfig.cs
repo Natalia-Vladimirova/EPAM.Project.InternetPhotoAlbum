564100b37a8b75cf857569dc70717603ffbdfffb
Natalia Vladimirova <dochka.tankista@gmail.com>
2016-06-11 19:37:07 +0300

    Added Ninject dependency resolver.

---
+++ DependencyResolver/ResolverConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using Ninject;
+using Ninject.Web.Common;
+using ORM;
+using DAL.Interfaces.Repositories;
+using DAL.Repositories;
+using BLL.Interfaces.Services;
+using BLL.Services;
+
+namespace DependencyResolver
+{
+    public static class ResolverConfig
+    {
+        public static void ConfigurateResolverWeb(this IKernel kernel)
+        {
+            Configure(kernel, true);
+        }
+
+        public static void ConfigurateResolverConsole(this IKernel kernel)
+        {
+            Configure(kernel, false);
+        }
+
+        private static void Configure(IKernel kernel, bool isWeb)
+        {
+            if (isWeb)
+            {
+                kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
+                kernel.Bind<DbContext>().To<ApplicationDbContext>().InRequestScope();
+            }
+            else
+            {
+                kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InSingletonScope();
+                kernel.Bind<DbContext>().To<ApplicationDbContext>().InSingletonScope();
+            }
+
+            kernel.Bind<IUserRepository>().To<UserRepository>();
+            kernel.Bind<IUserService>().To<UserService>();
+        }
+    }
+}
